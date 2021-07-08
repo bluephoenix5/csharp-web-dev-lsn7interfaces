a55f0481dcecd8b6214515a9327d863778220bce
bluephoenix5 <mia.a.lord@gmail.com>
2021-07-08 15:04:15 -0700

    Final

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            int xInt = (int)(x.Cost * 100);           
+            int yInt = (int)(y.Cost * 100);          
+            return xInt - yInt;
+
+            int xIn = xInt;
+            double xOut = Convert.ToDouble(xIn);
+
+            int yIn = yInt;
+            double yOut = Convert.ToDouble(yIn);
+        }
+
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -10,14 +10,33 @@ namespace IceCreamShop
             Case menu = new Case();
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
+            availableFlavors.Sort(new FlavorComparer());
+            availableCones.Sort(new ConeComparer());
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+           
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+            //  
+                        
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+            foreach(var flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+            Console.WriteLine();
+
+            foreach(var cone in availableCones)
+            {
+                Console.WriteLine(cone.Cost);
+            }
+
+
         }
     }
 }
